8a03f2e2fb84e8c2f9a522f95ea7430189faaa2c
MohamadTarhini84 <moudi55.mt@gmail.com>
2022-11-26 15:08:18 +0200

    made show/hide password feature, started to improve code redundancy

---
+++ Login/JavaScript/hidePassword.js
@@ -0,0 +1,15 @@
+let eye=document.getElementsByClassName('signup_eye');
+let passInput=document.getElementById('passInput');
+
+eye[0].addEventListener('click',function (e){
+    // console.log(e.target.classList.contains("fa-eye-slash"))
+    if(e.target.classList.contains('fa-eye-slash')){
+        e.target.setAttribute("class","fa fa-eye signup_eye");
+        passInput.setAttribute('type','text');
+    } else{
+        e.target.setAttribute("class","fa fa-eye-slash signup_eye");
+        passInput.setAttribute('type','password');
+    }
+});
+
+console.log("a");
