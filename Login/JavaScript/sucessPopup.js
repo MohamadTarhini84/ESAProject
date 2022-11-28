ed5f3eadee133232c120b4710d11a4ce2e2324e6
MohamadTarhini84 <moudi55.mt@gmail.com>
2022-11-28 09:26:09 +0200

    cleaned code, configured popup on form submit

--- Login/JavaScript/sucessPopup.js
+++ Login/JavaScript/sucessPopup.js
@@ -1,5 +1,16 @@
 let signupButton=document.getElementById('signup_button');
 let check=document.getElementById('popup');
+let myFrom=document.getElementsByTagName('form');
+var submitTimer;
+
+myFrom[0].addEventListener('submit',submitHandler);
+
+function submitHandler(event) {
+    event.preventDefault();
+    submitTimer = setTimeout(() => {
+    this.submit();
+    }, 2000)
+};
 
 signupButton.addEventListener('click', function (){
         check.setAttribute('style','display:flex');
