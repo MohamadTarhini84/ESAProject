36c3de4593349b729346eb60ca5a5d3fddca7782
yasser traboulsi <yassertraboulsi@gmail.com>
2022-12-13 21:51:25 +0200

    delet book

--- book/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "abcd")
-
-    or die("<p>the database server is not available.</p>");
-
-// @mysqli_select_db($conn, "filtersdb")
-//     or die("<p>the database is not available</p>");
