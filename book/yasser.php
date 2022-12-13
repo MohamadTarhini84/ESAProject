b62541b4815ea4020519c6ef3ab6be3473cfc8ce
yasser traboulsi <yassertraboulsi@gmail.com>
2022-12-13 19:12:27 +0200

    new links

--- book/yasser.php
+++ book/yasser.php
@@ -54,13 +54,13 @@
                     </a>
                     <div class="menu">
                         <ul>
-                            <li><a href="../index.html">Home</a> </li>
+                            <li><a href="../index.php">Home</a> </li>
                             <li><a href="../services/ser.html">Services</a> </li>
                             <li><a href="./yasser.php">Book</a> </li>
                             <li><a href="../blog/blog.html">Blog</a> </li>
                             <li><a href="../doctors/Doctors23.html">Doctors</a> </li>
                             <li><a href="../about/About.html">About</a> </li>
-                            <li><a href="../newcontact/contact.html">Contact</a> </li>
+                            <li><a href="../newcontact/contact.php">Contact</a> </li>
 
                         </ul>
                     </div>
