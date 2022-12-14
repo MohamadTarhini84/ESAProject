1e8984469ca129e3b75c4e0d0cb6cd4da2b4def6
yasser traboulsi <yassertraboulsi@gmail.com>
2022-12-14 17:04:50 +0200

    login

--- index.php
+++ index.php
@@ -979,14 +979,14 @@ session_start();
 
     // make 
     function makebtt() {
-        const url = './book/yasser.html';
+        const url = './book/book.php';
         window.location.replace(url);
     }
 
 
     //login
     function loginbtt() {
-        const url = './ESAProject/loginn/newSignIN.php';
+        const url = './loginn/newSignIN.php';
         window.location.replace(url);
     }
 
